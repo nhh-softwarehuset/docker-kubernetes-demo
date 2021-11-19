14d481ad9e7fc33d68766385de7f478c0a764e53
Nicolaj Helmer Hartmann <nhh@softwarehuset.com>
2021-11-19 01:52:59 +0100

    initial hello dotnet core example

---
+++ Hello-dotnet-core/Hello-dotnet-core/CakeResult.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Hello_dotnet_core
+{
+    public class CakeResult
+    {
+        public string Title { get; set; }
+        public string Conclusion { get; set; }
+    }
+}
---
+++ Hello-dotnet-core/Hello-dotnet-core/Controllers/CakeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hello_dotnet_core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CakeController : ControllerBase
+    {
+
+        private readonly ILogger<CakeController> logger;
+
+        public CakeController(ILogger<CakeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetCakeConclusion()
+        {
+            return Ok(new CakeResult()
+            {
+                Title = "Who Provides cake today?",
+                Conclusion = "Morten Storm"
+            });
+        }
+    }
+}
